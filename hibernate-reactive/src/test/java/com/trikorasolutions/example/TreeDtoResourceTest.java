94b2d304f3f546064599ead4b12f5e57e27048a6
A.C <ajc101@trikorasolutions.com>
2022-03-10 11:31:26 +0100

    chore: reformat

--- hibernate-reactive/src/test/java/com/trikorasolutions/example/TreeDtoResourceTest.java
+++ hibernate-reactive/src/test/java/com/trikorasolutions/example/TreeDtoResourceTest.java
@@ -1,34 +1,39 @@
 package com.trikorasolutions.example;
 
-import com.trikorasolutions.example.dto.FruitDto;
-import com.trikorasolutions.example.dto.TreeDto;
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
 
+import com.trikorasolutions.example.dto.FruitDto;
+import com.trikorasolutions.example.dto.TreeDto;
+import io.quarkus.test.junit.QuarkusTest;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @QuarkusTest
 public class TreeDtoResourceTest {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(TreeDtoResourceTest.class);
+
+  @Inject
+  private FruitResources fruitResources;
 
   @Test
   public void getTreeOk() {
     final TreeDto tree = new TreeDto("tree_get_ok", null);
 
     given().when().body(tree).contentType(MediaType.APPLICATION_JSON).post("/tree/create").then()
-      .statusCode(OK.getStatusCode()).body("name", is(tree.name));
+        .statusCode(OK.getStatusCode()).body("name", is(tree.name));
 
-    given().when().get(String.format("/tree/name/%s", tree.name)).then().statusCode(OK.getStatusCode())
-      .body("name", is(tree.name));
+    given().when().get(String.format("/tree/name/%s", tree.name)).then()
+        .statusCode(OK.getStatusCode())
+        .body("name", is(tree.name));
   }
 
   @Test
@@ -58,18 +63,23 @@ public class TreeDtoResourceTest {
 
     // Ensure the tree is persisted
     given().when().body(tree).contentType(MediaType.APPLICATION_JSON).post("/tree/create").then()
-      .statusCode(OK.getStatusCode()).body("name", is("createTreeWithFruitsOk"), "fruits.size()", is(fruits.size()));
+        .statusCode(OK.getStatusCode())
+        .body("name", is("createTreeWithFruitsOk"), "fruits.size()", is(fruits.size()));
 
     // Ensure that the full tree is persisted
-    given().when().get(String.format("/tree/name/%s", tree.name)).then().statusCode(OK.getStatusCode())
-      .body("name", is("createTreeWithFruitsOk"));
+    given().when().get(String.format("/tree/name/%s", tree.name)).then()
+        .statusCode(OK.getStatusCode())
+        .body("name", is("createTreeWithFruitsOk"));
 
     // Ensure that the fruits persisted when persisting the tree are in th db
-    given().when().get("/fruitreact/name/orange").then().statusCode(OK.getStatusCode()).body("name", is("orange"));
-    given().when().get("/fruitreact/name/Carrot").then().statusCode(OK.getStatusCode()).body("name", is("Carrot"));
+    fruitResources.get("orange").then().statusCode(OK.getStatusCode())
+        .body("name", is("orange"));
+    fruitResources.get("Carrot").then().statusCode(OK.getStatusCode())
+        .body("name", is("Carrot"));
 
     // Ensure that the full tree is persisted
-    given().when().get(String.format("/tree/getFull/name/%s", tree.name)).then().statusCode(OK.getStatusCode())
-      .body("name", is("createTreeWithFruitsOk"), "fruits.size()", is(fruits.size()));
+    given().when().get(String.format("/tree/getFull/name/%s", tree.name)).then()
+        .statusCode(OK.getStatusCode())
+        .body("name", is("createTreeWithFruitsOk"), "fruits.size()", is(fruits.size()));
   }
 }
